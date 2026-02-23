d2859debfe3161c2ccfd75178f9818602bb0fe38
samhercom <samhercom@gmail.com>
2026-02-23 16:05:15 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfitMarginCalculator from "./components/ProfitMarginCalculator.jsx";
+
+export default function App() {
+  return (
+    <div className="appShell">
+      <ProfitMarginCalculator />
+    </div>
+  );
+}
