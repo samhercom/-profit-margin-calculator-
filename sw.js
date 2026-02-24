086eb7f610a6834f4c376c88e7e0a1ff1e5f14c6
samhercom <samhercom@gmail.com>
2026-02-23 18:39:11 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open('pmc').then(c=>c.addAll(['./'])))
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+});
